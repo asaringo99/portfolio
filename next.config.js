bd2219f54ce14e63fc363c62f76c5bca376d7bf3
asaringo99 <ka99code@gmail.com>
2023-06-06 21:04:37 +0900

    nextconfig

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-    'process.env.ASSETS_URI': isProd ? '/portfolio' : '',
+    'process.env.ASSETS_URI' : isProd ? '/portfolio' : '',
     basePath: process.env.GITHUB_ACTIONS && "/portfolio",
     trailingSlash: true,
     output: 'export',
