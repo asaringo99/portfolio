16a7f4d55ae2955cbb0048ad1ec167c4fce77dfa
asaringo99 <ka99code@gmail.com>
2023-06-06 17:32:38 +0900

    [add] output: export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: process.env.GITHUB_ACTIONS && "/portfolio",
-    // trailingSlash: true,
+    basePath: process.env.GITHUB_ACTIONS && "/portfolio",
+    trailingSlash: true,
+    output: 'export',
 }
 
 module.exports = nextConfig
